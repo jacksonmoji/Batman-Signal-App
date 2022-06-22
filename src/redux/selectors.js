3a1e18bf46ef3d4dca2e5780f3f9741a4b01173f
Jackson Moji <jacksonmoji@gmail.com>
2022-06-22 21:39:57 +0200

    switched to all state fetch from authReducers

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,6 @@
 export const getPanicHistory = (state) => state.panicReducers.panicList;
 export const loading = (state) => state.loadingReducers.loading;
-export const getAuthenticationStatus = (state) =>
-  state.authReducers.isAuthenticated;
+export const getAuthenticationStatus = (state) => state.authReducers;
 
 export const getNotification = (state) => state.notificationReducers;
 export const getPanicInformation = (state) => state.panicReducers;
